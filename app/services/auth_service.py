f277efee57eed4af68fe04caf13d70407f0af3dd
Amy <104085802+Amy2000X@users.noreply.github.com>
2026-06-18 21:46:31 +0200

    Create repository layer
    -added email verification in auth_service
    -separate logic and database to service and repository

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,21 +1,23 @@
-from app.database import supabase
+from app.repositories import auth_repository
+from email_validator import validate_email, EmailNotValidError
 
 def register_user(email: str, password: str):
-    response = supabase.auth.sign_up(
-        {
-            "email": email,
-            "password": password
-        }
-    )
-    
+    new_email = is_email_valid(email.lower())
+    response = auth_repository.register_user(new_email, password)
+
     return response
 
 def login_user(email: str, password: str):
-    response = supabase.auth.sign_in_with_password(
-        {
-            "email": email,
-            "password": password,
-        }
-    )
+    new_email = is_email_valid(email.lower())
+    response = auth_repository.login_user(new_email, password)
 
     return response
+
+
+def is_email_valid(email: str):
+    try:
+        result = validate_email(email)
+        return result.normalized
+    except EmailNotValidError as e:
+        raise ValueError(e)
+    
